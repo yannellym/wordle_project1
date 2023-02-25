78f69a47cae33c03d0f24ebaf65a2f51ae5daaf4
yannelly25 <yannellym@gmail.com>
2023-02-25 17:36:08 -0500

    final completed

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.wordle
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -20,9 +18,10 @@ class MainActivity : AppCompatActivity() {
     )
     // variables that wont be changing or change very little
     private var attempt = 1
-    private var correctGuesses = 0
     private val toolbox = FourLetterWordList()
+    // turn our word to lowercase so the user doesn't have to write in uppercase
     private var chosenWord = toolbox.getRandomFourLetterWord().lowercase()
+    // lateinit modifier: https://kotlinlang.org/docs/properties.html#late-initialized-properties-and-variables
     private lateinit var inputBox: EditText
     private lateinit var winMessage: TextView
     private lateinit var button: Button
@@ -41,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         resetGame()
         // sets our button to listen to a click
         button.setOnClickListener {
+            // gets the input from the user
             val userInput = inputBox.text
             // if the userInput is less than the length of the chosen word's length,
             // display a toast saying that the input must be 4 letters long
@@ -50,17 +50,22 @@ class MainActivity : AppCompatActivity() {
                     "GUESS MUST BE ${chosenWord.length} LETTERS LONG",
                     Toast.LENGTH_SHORT
                 ).show()
+                // if the attempt is less than or equal to 3
             } else if (attempt <= 3) {
+                // check how many letters the user got correct
                 val update = checkGuess(chosenWord, userInput)
+                // get the fields that correspond to our current guess Ex, guess 0,1,or 2
                 val (field, check) = fieldIds[attempt - 1]
+                // reference our fields
                 val fieldView = findViewById<TextView>(field)
                 val checkView = findViewById<TextView>(check)
+                // update our fields
                 fieldView.text = userInput
                 checkView.text = update
                 // increase attempt to +1
                 attempt++
                 // if the checked guess is "0000" they have guessed the correct word!
-                if (update == "OOOO") {
+                if (update == "✅✅✅✅") {
                     // display the chosen word along with two party emojis
                     winMessage.setTextColor(Color.MAGENTA)
                     winMessage.text = "\uD83C\uDF89  ${chosenWord.uppercase()}  \uD83C\uDF89"
@@ -73,19 +78,15 @@ class MainActivity : AppCompatActivity() {
                     disableButton()
                     // let the user know the game is over by displaying a toast with "game over"
                     Toast.makeText(this, "GAME OVER", Toast.LENGTH_SHORT).show()
-                } else if (update == "OOO") { // added condition to count correct guesses
-                    // this will update how many times the user has guessed the correct word
-                    correctGuesses++
-                    Toast.makeText(this, "CORRECT!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-
+        // when the user clicks on the reset button, call the resetGame function
         resetBtn.setOnClickListener {
             resetGame()
         }
     }
-
+    // function keeps track of the attempts, the chosen word, the user's input, and the field
     private fun resetGame() {
         attempt = 1
         chosenWord = toolbox.getRandomFourLetterWord().lowercase()
@@ -105,18 +106,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkGuess(chosenWord: String, guess: CharSequence): String {
         // checks each letter in the guess word to see if the user has guessed a letter correctly
-        // if they user guessed a letter and its matches the letter in the chosen word's index -> 0
-        // if they user guessed a letter and it doesnt match the letter in the chosen word's index and its not in chosen word -> X
-        // if they user guessed a letter and it doesnt matche the letter in the chosen word's index but it is in chosen word -> +
+        // if they user guessed a letter and its matches the letter in the chosen word's index -> ✅
+        // if they user guessed a letter and it doesn't match the letter in the chosen word's index and its not in chosen word -> ❕
+        // if they user guessed a letter and it doesn't match the letter in the chosen word's index but it is in chosen word -> ❌
+        //MapIndexed returns a list containing the results of applying the given transform function to each character and its index in the original char sequence
         return guess.mapIndexed { index, c ->
             when {
-                c == chosenWord[index] -> "O"
-                c in chosenWord -> "+"
-                else -> "X"
+                c == chosenWord[index] -> "✅"
+                c in chosenWord -> "❕"
+                else -> "❌"
             }
         }.joinToString("")
     }
-
+    // disables the submit button by making it invisible and turns the reset button visible
     private fun disableButton() {
         // function to disable/enable the buttons
         button.visibility = View.INVISIBLE
@@ -124,6 +126,6 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
+// features I wish I could have added: adding the Confetti library, and changing the color of each individual letter when it was guessed
 
 //    Toast.makeText(this, "WRONG  no ${userInput[i]}", Toast.LENGTH_SHORT).show()
